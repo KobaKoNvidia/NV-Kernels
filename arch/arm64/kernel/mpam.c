700b25f26000e7e263262fc5ae74dcdacc70075a
James Morse <james.morse@arm.com>
2021-06-25 13:29:39 +0100

    arm_mpam: resctrl: Allow resctrl to allocate monitors
    When resctrl wants to read a domain's 'QOS_L3_OCCUP', it needs
    to allocate a monitor on the corresponding resource. Monitors are
    allocated by class instead of component.
    
    Add helpers to do this.
    
    The MBM events depend on having their monitors allocated at init
    time so that they can be left running. The value USE_RMID_IDX
    is out of range for a monitor, and is used to indicate this
    behaviour.
    
    resctrl_arch_mon_ctx_alloc() is implemented to have a no_wait
    version and a waitqueue for callers that sleep. The no_wait
    version will later become an interface for the resctrl_pmu
    to use.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    (backported from commit 0e131acd37ebe32adab83e1d2200dd6205b7683f git://git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git)
    Signed-off-by: KobaK <kobak@nvidia.com>

--- arch/arm64/kernel/mpam.c
+++ arch/arm64/kernel/mpam.c
@@ -8,6 +8,7 @@
 #include <linux/percpu.h>
 
 DEFINE_STATIC_KEY_FALSE(arm64_mpam_has_hcr);
+DEFINE_STATIC_KEY_FALSE(mpam_enabled);
 DEFINE_PER_CPU(u64, arm64_mpam_default);
 DEFINE_PER_CPU(u64, arm64_mpam_current);
 
