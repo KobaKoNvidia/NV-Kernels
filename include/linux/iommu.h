d348e6e019e17d565575769aa9c1286e2eee2f5a
James Morse <james.morse@arm.com>
2021-09-16 16:50:46 +0100

    arm_mpam: resctrl: Add iommu helpers to get/set the partid and pmg
    SMMU that support MPAM can be configured to use a particular partid
    and pmg for a stream. The assignment of an iommu_group and its
    corresponding streams should be done via resctrl.
    
    Add helpers similar to setting a closid/rmid on a task. We need
    the same shifting if the CPUs are using CDP. The SMMU only takes
    one partid, conceptually its always making data accesses.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: KobaK <kobak@nvidia.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -909,6 +909,8 @@ extern struct iommu_group *iommu_group_ref_get(struct iommu_group *group);
 extern void iommu_group_put(struct iommu_group *group);
 
 extern int iommu_group_id(struct iommu_group *group);
+struct kset *iommu_get_group_kset(void);
+const struct iommu_ops *iommu_group_get_ops(struct iommu_group *group);
 extern struct iommu_domain *iommu_group_default_domain(struct iommu_group *);
 
 int iommu_set_pgtable_quirks(struct iommu_domain *domain,
