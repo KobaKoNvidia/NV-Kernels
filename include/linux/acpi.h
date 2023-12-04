18e2a6f647431d13dd830a29e11e866d335ba59d
James Morse <james.morse@arm.com>
2023-12-04 14:40:25 +0000

    ACPI / PPTT: Add a helper to fill a cpumask from a processor container
    The ACPI table for MPAM describes a set of CPUs with the UID of a
    processor container. These exist both in the namespace and the PPTT.
    
    Using the existing for-each helpers, provide a helper to find the
    specified processor container in the PPTT, and fill a cpumask
    with the CPUs that belong to it.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    (backported from commit 3c920715e3e3bb5cd28b697db9103e482359861a git://git.kernel.org/pub/scm/linux/kernel/git/morse/linux.git)
    Signed-off-by: KobaK <kobak@nvidia.com>

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -1498,6 +1498,8 @@ int find_acpi_cpu_topology_hetero_id(unsigned int cpu);
 int find_acpi_cpu_cache_topology(unsigned int cpu, int level);
 u32 acpi_pptt_count_containers(void);
 int acpi_pptt_for_each_container(acpi_pptt_cpu_callback_t callback, void *arg);
+int acpi_pptt_get_cpus_from_container(u32 acpi_cpu_id, cpumask_t *cpus);
+void acpi_pptt_get_child_cpus(struct acpi_pptt_processor *parent_node, cpumask_t *cpus);
 #else
 static inline int acpi_pptt_cpu_is_thread(unsigned int cpu)
 {
@@ -1531,6 +1533,16 @@ static inline int
 acpi_pptt_for_each_container(acpi_pptt_cpu_callback_t *callback, void *arg)
 {
        return -EINVAL;
+}
+static inline int acpi_pptt_get_cpus_from_container(u32 acpi_cpu_id,
+						    cpumask_t *cpus)
+{
+	return -EINVAL;
+}
+static inline void
+acpi_pptt_get_child_cpus(struct acpi_pptt_processor *parent_node,
+                        cpumask_t *cpus)
+{
 }
 #endif
 
