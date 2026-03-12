kernelorg-mirror/linux_kernel_git_morse_linux

arm_mpam: Allow cmax/cmin to be configured
mpam_reprogram_ris_partid() always resets the CMAX/CMIN controls to their
'unrestricted' value.

This prevents the controls from being configured.

Add fields in struct mpam_config, and program these values when they
are set in the features bitmask.

Signed-off-by: James Morse <james.morse@arm.com>

drivers/platform/arm64/mpam/mpam_devices.c
@@ -1693,11 +1693,25 @@ static void mpam_reprogram_ris_partid(struct mpam_msc_ris *ris, u16 partid,
 	if (mpam_has_feature(mpam_feat_mbw_prop, rprops))
 		mpam_write_partsel_reg(msc, MBW_PROP, bwa_fract);
 
-	if (mpam_has_feature(mpam_feat_cmax_cmax, rprops))
-		mpam_write_partsel_reg(msc, CMAX, cmax);
+	if (mpam_has_feature(mpam_feat_cmax_cmax, rprops)) {
+		if (mpam_has_feature(mpam_feat_cmax_cmax, cfg)) {
+			u32 cmax_val = cfg->cmax;
 
-	if (mpam_has_feature(mpam_feat_cmax_cmin, rprops))
-		mpam_write_partsel_reg(msc, CMIN, 0);
+			if (cfg->cmax_softlim)
+				cmax_val |= MPAMCFG_CMAX_SOFTLIM;
+			mpam_write_partsel_reg(msc, CMAX, cmax_val);
+		} else {
+			mpam_write_partsel_reg(msc, CMAX, cmax);
+		}
+	}
+
+	if (mpam_has_feature(mpam_feat_cmax_cmin, rprops)) {
+		if (mpam_has_feature(mpam_feat_cmax_cmin, cfg)) {
+			mpam_write_partsel_reg(msc, CMIN, cfg->cmin);
+		} else {
+			mpam_write_partsel_reg(msc, CMIN, 0);
+		}
+	}
 
 	if (mpam_has_feature(mpam_feat_intpri_part, rprops) ||
 	    mpam_has_feature(mpam_feat_dspri_part, rprops)) {
@@ -3215,6 +3229,7 @@ static mpam_features_t mpam_update_config(struct mpam_config *cfg,
 	mpam_features_t changes = 0;
 
 	maybe_update_config(cfg, mpam_feat_cpor_part, newcfg, cpbm, changes);
+	maybe_update_config(cfg, mpam_feat_cmax_cmax, newcfg, cmax, changes);
 	maybe_update_config(cfg, mpam_feat_mbw_part, newcfg, mbw_pbm, changes);
 	maybe_update_config(cfg, mpam_feat_mbw_max, newcfg, mbw_max, changes);
 

KobaKoNvidia/NV-Kernels

Signed-off-by: Shanker Donthineni <sdonthineni@nvidia.com>

drivers/resctrl/mpam_devices.c
@@ -1612,11 +1612,25 @@ static void mpam_reprogram_ris_partid(struct mpam_msc_ris *ris, u16 partid,
 	    mpam_has_feature(mpam_feat_mbw_prop, cfg))
 		mpam_write_partsel_reg(msc, MBW_PROP, 0);
 
-	if (mpam_has_feature(mpam_feat_cmax_cmax, rprops))
-		mpam_write_partsel_reg(msc, CMAX, cmax);
+	if (mpam_has_feature(mpam_feat_cmax_cmax, rprops)) {
+		if (mpam_has_feature(mpam_feat_cmax_cmax, cfg)) {
+			u32 cmax_val = cfg->cmax;
 
-	if (mpam_has_feature(mpam_feat_cmax_cmin, rprops))
-		mpam_write_partsel_reg(msc, CMIN, 0);
+			if (cfg->cmax_softlim)
+				cmax_val |= MPAMCFG_CMAX_SOFTLIM;
+			mpam_write_partsel_reg(msc, CMAX, cmax_val);
+		} else {
+			mpam_write_partsel_reg(msc, CMAX, cmax);
+		}
+	}
+
+	if (mpam_has_feature(mpam_feat_cmax_cmin, rprops)) {
+		if (mpam_has_feature(mpam_feat_cmax_cmin, cfg)) {
+			mpam_write_partsel_reg(msc, CMIN, cfg->cmin);
+		} else {
+			mpam_write_partsel_reg(msc, CMIN, 0);
+		}
+	}
 
 	if (mpam_has_feature(mpam_feat_cmax_cassoc, rprops))
 		mpam_write_partsel_reg(msc, CASSOC, MPAMCFG_CASSOC_CASSOC);
@@ -3049,6 +3063,7 @@ static bool mpam_update_config(struct mpam_config *cfg,
 	bool has_changes = false;
 
 	maybe_update_config(cfg, mpam_feat_cpor_part, newcfg, cpbm, has_changes);
+	maybe_update_config(cfg, mpam_feat_cmax_cmax, newcfg, cmax, has_changes);
 	maybe_update_config(cfg, mpam_feat_mbw_part, newcfg, mbw_pbm, has_changes);
 	maybe_update_config(cfg, mpam_feat_mbw_max, newcfg, mbw_max, has_changes);
 